82fc7fea9aa412a423e02a3019db5826522b5a27
lucasbardoui <71636800+lucasbardoui@users.noreply.github.com>
2021-12-21 11:26:43 +0100

    Add files via upload

---
+++ Burger.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+interface IBurger
+{
+    string GetBurgerType();
+}
+
+class Burger : IBurger
+{
+    public virtual 
+        string GetBurgerType()
+    {
+        return "Burger normal";
+    }
+}
+
+class BurgerBeef: Burger
+{
+    public override string GetBurgerType()
+    {
+        return "Burger au boeuf";
+    }
+}
+
+class BurgerChicken : Burger
+{
+    public override string GetBurgerType()
+    {
+        return "Burger au poulet";
+    }
+}
+
+class BurgerDecorator : IBurger
+{
+    private IBurger _burger;
+
+    public BurgerDecorator(IBurger burger)
+    {
+        _burger = burger;
+    }
+    public virtual string GetBurgerType()
+    {
+        return _burger.GetBurgerType();
+    }
+}
+
+class SaladDecorator : BurgerDecorator
+{
+    public SaladDecorator(IBurger burger) : base(burger)
+    {
+
+    }
+    public override string GetBurgerType()
+    {
+        string type = base.GetBurgerType();
+        type += "\n ajout salade";
+        return type;
+    }
+}
+
+class TomatoDecorator : BurgerDecorator
+{
+    public TomatoDecorator(IBurger burger) : base(burger)
+    {
+
+    }
+    public override string GetBurgerType()
+    {
+        string type = base.GetBurgerType();
+        type += "\n ajout tomate";
+        return type;
+    }
+}
+
+class BeefDecorator : BurgerDecorator
+{
+    public BeefDecorator(IBurger burger) : base(burger)
+    {
+
+    }
+    public override string GetBurgerType()
+    {
+        string type = base.GetBurgerType();
+        type += "\n extra boeuf";
+        return type;
+    }
+}
+
+class ChickenFriedDecorator : BurgerDecorator
+{
+    public ChickenFriedDecorator(IBurger burger) : base(burger)
+    {
+
+    }
+    public override string GetBurgerType()
+    {
+        string type = base.GetBurgerType();
+        type += "\n extra poulet pané";
+        return type;
+    }
+}
+
+class WrappedDecorator : BurgerDecorator
+{
+    public WrappedDecorator(IBurger burger) : base(burger)
+    {
+
+    }
+    public override string GetBurgerType()
+    {
+        string type = base.GetBurgerType();
+        type += "\n Le burger est emballé";
+        return type;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        IBurger beef = new BurgerBeef();
+        IBurger addtomato = new TomatoDecorator(beef);
+        IBurger addsalad = new SaladDecorator(addtomato);
+        IBurger extrabeef = new BeefDecorator(addsalad);
+        IBurger wrappbeef = new WrappedDecorator(extrabeef);
+        Console.WriteLine(wrappbeef.GetBurgerType());
+
+
+        IBurger chicken = new BurgerChicken();
+        IBurger chickentomato = new TomatoDecorator(chicken);
+        IBurger chickensalad = new SaladDecorator(chickentomato);
+        IBurger addchickenfried = new BeefDecorator(chickensalad);
+        IBurger wrappchicken = new WrappedDecorator(addchickenfried);
+        Console.WriteLine(wrappchicken.GetBurgerType());
+    }
+    
+}
